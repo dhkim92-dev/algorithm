fe6340b22c563521fc314be9f84eb03ce84145e5
dohoon <dhkim92@nalbi.ai>
2023-02-01 22:50:31 +0900

    프로그래머스 lv3 억억단을 외우자 풀이 추가

---
+++ 프로그래머스/lv3/억억단을_외우자/main.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Divisors{
+    int number;
+    int count;
+
+    bool operator< (const Divisors& nxt) const {
+        if(number != nxt.number) return count > nxt.count;
+        return number < nxt.number;
+    }
+};
+
+vector<int> solution(int e, vector<int> starts) {
+    vector<int> answer;
+    vector<Divisors> divisors(e+1);
+
+    for(int i = 1 ; i <= e ; i++){
+        divisors[i].number = i;
+        divisors[i].count = 2; // 1과 자기자신으로 무조건 나누어짐
+    }
+
+    for(int i = 2 ; i <= e ; i++){
+        for(int j = i ; j <= e ; j++){
+            long long limit = i*j;
+            if(limit > e) break;
+            divisors[limit].number = static_cast<int>(limit);
+            divisors[limit].count += (i == j) ? 1 : 2;
+        }
+    }
+
+    sort(divisors.begin() + 1, divisors.end());
+
+    for(int s : starts){
+        for(int i = 1 ; i<=e ; i++){
+            if(divisors[i].number >= s && divisors[i].number <= e){
+                answer.push_back(divisors[i].number);
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
