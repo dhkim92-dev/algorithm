fd3322183643e88636f79daa5225f85937a0807e
dohoon <elensar92@gmail.com>
2023-09-07 02:38:29 +0900

    백준 2631 줄세우기 풀이 추가

---
+++ 백준/2631_줄세우기/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+    int N;
+    vector<int> targets;
+    vector<int> dp;
+    int answer = 1e9;
+    
+    void init() {
+        cin >> N;
+        targets.resize(N+1, 0);
+        dp.resize(N+1, 1);
+
+        for(int i = 0 ; i < N ; i++) {
+            cin >> targets[i+1];
+        } 
+    }
+
+    void print() {
+        cout << answer << endl;
+    }
+
+    void simulate() {
+        for(int i = 1 ; i <= N ; i++) {
+            for(int j = 1 ; j <= i ; j++) {
+                if(targets[j] < targets[i]) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+        }
+
+        sort(dp.begin(), dp.end());
+        answer = N - dp.back();
+    }
+
+public :
+    void run() {
+        init();
+        simulate();
+        print();
+    }
+};
+
+int main(void)
+{
+    Solution sol;
+    sol.run();
+    return 0;
+}
