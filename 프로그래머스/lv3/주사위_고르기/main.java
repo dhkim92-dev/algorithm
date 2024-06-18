1e857d4fef5c56c8099706a760b2aa62d7804247
dohoon <elensar92@gmail.com>
2024-06-19 02:01:37 +0900

    주사위 고르기 풀이 수정

--- 프로그래머스/lv3/주사위_고르기/main.java
+++ 프로그래머스/lv3/주사위_고르기/main.java
@@ -1,4 +1,36 @@
+import java.util.*;
 
+class Solution {
+
+    private int[][] dice;
+    private int[] comb;
+    private int limit;
+    private int[] answer;
+    private List<int[]> diceCombs = new ArrayList<>();
+
+    private int countWins(int target, List<Integer> scores) {
+        int left = 0;
+        int right = scores.size() - 1;
+        int mid = 0;
+
+        while(left <= right) {
+            mid = (left + right) / 2;
+            int value = scores.get(mid);
+
+            if(value < target) { // 현재 값이 검색하려는 값보다 작다면
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
+    private void makeScoreCombination(int[] indices, int start, int limit, int score, List<Integer> output) {
+        if(start == limit) {
+            output.add(score);
+            return;
         }
 
         for(int i = 0 ; i < 6 ; i++) {
@@ -28,7 +60,7 @@
         int maxWin = 0;
 
         // System.out.println("Score result ");
-        for(int i = 0 ; i < diceCombs.size() ; i++) {
+        for(int i = 0 ; i < diceCombs.size() / 2 ; i++) {
 
             int[] aDices = diceCombs.get(i);
             int[] bDices = diceCombs.get(diceCombs.size() - i - 1);
@@ -39,3 +71,38 @@
             makeScoreCombination(aDices, 0, limit/2, 0, aScore);
             makeScoreCombination(bDices, 0, limit/2, 0, bScore);
 
+            Collections.sort(aScore);
+            Collections.sort(bScore);
+
+            int aTotalWin = 0; // 승률은 중요하지 않다. 어차피 모두 같은 시행 회수이므로 승리 수만 합계하여 비교하면 된다.
+            int bTotalWin = 0; // 승률은 중요하지 않다. 어차피 모두 같은 시행 회수이므로 승리 수만 합계하여 비교하면 된다.
+            
+            for(int aValue : aScore) {
+                int winCount = countWins(aValue, bScore);
+                aTotalWin+=winCount;
+            }
+            
+            for(int bValue : bScore) {
+                int winCount = countWins(bValue, aScore);
+                bTotalWin += winCount;
+            }
+            
+
+            if(aTotalWin > maxWin) {
+                maxWin = aTotalWin;
+                for(int dIdx = 0 ; dIdx < limit/2 ; dIdx++) {
+                    answer[dIdx] = aDices[dIdx] + 1;
+                }
+            }
+            
+            if(bTotalWin > maxWin) {
+                maxWin = bTotalWin;
+                for(int dIdx = 0 ; dIdx < limit/2 ; dIdx++) {
+                    answer[dIdx] = bDices[dIdx] + 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
